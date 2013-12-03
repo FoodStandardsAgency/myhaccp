f2a35f8dc9661a0a33c3dd1214eedc38e6cd6fe4
Hedley Smith <hedley@agilecollective.com>
2013-12-03 14:22:18 +0000

    Adding SASS to My HACCP theme and making a few amendments to get the basics working

--- web/sites/all/themes/custom/myhaccp/page.tpl.php
+++ web/sites/all/themes/custom/myhaccp/page.tpl.php
@@ -11,84 +11,93 @@
 <?php print $cleanup_poorthemers_helper; ?>
 
 <header role="banner">
-  <div class="siteinfo">
-    <?php if ($logo): ?>
-      <figure>
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home">
-        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-      </a>
-      </figure>
-    <?php endif; ?>
+  <div class="header-wrapper">
+    <div class="header-inside">
 
-    <?php if($site_name OR $site_slogan ): ?>
-    <hgroup>
-      <?php if($site_name): ?>
-        <h1><?php print $site_name; ?></h1>
-      <?php endif; ?>
-      <?php if ($site_slogan): ?>
-        <h2><?php print $site_slogan; ?></h2>
-      <?php endif; ?>
-    </hgroup>
-    <?php endif; ?>
-  </div>
+      <div class="site-details">
+        <?php if ($logo): ?>
+          <div class="logo">
+          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home">
+            <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+          </a>
+          </div>
+        <?php endif; ?>
 
-  <?php if($page['header']): ?>
-    <div class="header-region">
-      <?php print render($page['header']); ?>
+        <?php if($site_name): ?>
+          <h1><?php print $site_name; ?></h1>
+        <?php endif; ?>
+        <?php if ($site_slogan): ?>
+          <h2><?php print $site_slogan; ?></h2>
+        <?php endif; ?>
+
+      </div>
+
+      <?php if($page['header']): ?>
+        <div class="header-region">
+          <?php print render($page['header']); ?>
+        </div>
+      <?php endif; ?>
+
     </div>
-  <?php endif; ?>
-
+  </div>
 </header>
 
 <div class="page">
 
-  <div role="main" id="main-content">
-    <?php print render($title_prefix); ?>
-    <?php if ($title): ?>
-      <h1><?php print $title; ?></h1>
-    <?php endif; ?>
-    <?php print render($title_suffix); ?>
+  <div class="main-wrapper">
 
-    <?php print $breadcrumb; ?>
+    <div role="main" id="main-content">
+      <?php print render($title_prefix); ?>
+      <?php if ($title): ?>
+        <h1><?php print $title; ?></h1>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
 
-    <?php if ($action_links): ?>
-      <ul class="action-links"><?php print render($action_links); ?></ul>
-    <?php endif; ?>
+      <?php print $breadcrumb; ?>
 
-    <?php if (isset($tabs['#primary'][0]) || isset($tabs['#secondary'][0])): ?>
-      <nav class="tabs"><?php print render($tabs); ?></nav>
-    <?php endif; ?>
+      <?php if ($action_links): ?>
+        <ul class="action-links"><?php print render($action_links); ?></ul>
+      <?php endif; ?>
 
-    <?php if($page['highlighted'] OR $messages){ ?>
-      <div class="drupal-messages">
-      <?php print render($page['highlighted']); ?>
-      <?php print $messages; ?>
+      <?php if (isset($tabs['#primary'][0]) || isset($tabs['#secondary'][0])): ?>
+        <nav class="tabs"><?php print render($tabs); ?></nav>
+      <?php endif; ?>
+
+      <?php if($page['highlighted'] OR $messages){ ?>
+        <div class="drupal-messages">
+        <?php print render($page['highlighted']); ?>
+        <?php print $messages; ?>
+        </div>
+      <?php } ?>
+
+
+      <?php print render($page['content_pre']); ?>
+
+      <?php print render($page['content']); ?>
+
+      <?php print render($page['content_post']); ?>
+
+    </div><!-- /main-->
+
+    <?php if ($page['sidebar_first']): ?>
+      <div class="sidebar-first">
+      <?php print render($page['sidebar_first']); ?>
       </div>
-    <?php } ?>
+    <?php endif; ?>
 
-
-    <?php print render($page['content_pre']); ?>
-
-    <?php print render($page['content']); ?>
-
-    <?php print render($page['content_post']); ?>
-
-  </div><!-- /main-->
-
-  <?php if ($page['sidebar_first']): ?>
-    <div class="sidebar-first">
-    <?php print render($page['sidebar_first']); ?>
-    </div>
-  <?php endif; ?>
-
-  <?php if ($page['sidebar_second']): ?>
-    <div class="sidebar-second">
-      <?php print render($page['sidebar_second']); ?>
-    </div>
-  <?php endif; ?>
+    <?php if ($page['sidebar_second']): ?>
+      <div class="sidebar-second">
+        <?php print render($page['sidebar_second']); ?>
+      </div>
+    <?php endif; ?>
+  </div>
 </div><!-- /page-->
 
 <footer role="contentinfo">
-  <?php print render($page['footer']); ?>
+  <div class="footer-wrapper">
+    <div class="footer-inside">
+      <?php print render($page['footer']); ?>
+    </div>
+  </div>
 </footer>
 
