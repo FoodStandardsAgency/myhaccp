a3b70ca28c4b0d82369a94d382a4338edd3063b7
Tanc <mail@tanc.me>
2014-01-10 10:10:09 +0000

    Added parsleyjs library and modified form validation so it makes use of parsley as well as storing validation server side.

---
+++ web/sites/all/modules/custom/myhaccp/mforms/js/parsley_custom.js
@@ -0,0 +1,23 @@
+(function($) {
+
+  Drupal.behaviors.parsley_custom = {
+    attach: function(context) {
+      var form = $('#mforms-wrapper-myhaccp form');
+      // Alter the default success and error classes.
+      form.parsley(
+        { successClass: 'success',
+          errorClass: 'error',
+          focus: 'none',
+          validateIfUnchanged: true
+        }
+      );
+      // Add a onFormValidate listener.
+      form.parsley('addListener', {
+        onFormValidate: function(elem) {
+          // Always validate, don't block form submission.
+          return true;
+        }
+      });
+    }
+  }
+})(jQuery);
