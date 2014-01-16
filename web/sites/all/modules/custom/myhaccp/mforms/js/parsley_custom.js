8839c18e8e82a660071997e8fb8973ba5b16d41a
Tanc <mail@tanc.me>
2014-01-16 14:08:03 +0000

    Added an error class handler to properly attach the error class to the parent of checkboxes.

--- web/sites/all/modules/custom/myhaccp/mforms/js/parsley_custom.js
+++ web/sites/all/modules/custom/myhaccp/mforms/js/parsley_custom.js
@@ -69,6 +69,18 @@
           },
           messages: {
             multiple: "This value should be a multiple of %s"
+          },
+          errors: {
+            classHandler: function(elem, isRadioOrCheckbox) {
+              // If this is a radio button or a checkbox then step up a couple
+              // of parents in the DOM to reach the wrapper.
+              if (isRadioOrCheckbox) {
+                return $(elem).parent().parent();
+              }
+              else {
+                return $(elem);
+              }
+            }
           }
         }
       );
