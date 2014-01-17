7cf6d113e2199bf1d971a6171ab47bf5de02ace0
Tanc <mail@tanc.me>
2014-01-17 16:04:39 +0000

    Fixed principle 1 question 2's javascript and form layout.

--- web/sites/all/modules/custom/myhaccp/mforms/js/principle_1_question_2.js
+++ web/sites/all/modules/custom/myhaccp/mforms/js/principle_1_question_2.js
@@ -8,33 +8,33 @@
   Drupal.behaviors.p1q2 = {
     attach: function(context) {
       // Calculate significance from the values in severity and likelihood.
-      $('.form-item.severity input, .form-item.likelihood input').change(function(e) {
+      $('input.severity, input.likelihood').bind('change, keyup', function(e) {
         var total = 0;
         // Fetch the row.
         var $row = $(this).parent().parent().parent();
         // Get the severity field value.
-        var severity = $row.find('.form-item.severity input').val();
+        var severity = $row.find('input.severity').val();
         // Check if severity is greater than three or less than one and if so
         // reset the field to empty.
         // @todo: Put in some kind of message.
         if (severity > 3 || severity < 1) {
-          $row.find('.form-item.severity input').val('');
+          $row.find('input.severity').val('');
           severity = 0;
         }
         // Get the likelihood field value.
-        var likelihood = $row.find('.form-item.likelihood input').val();
+        var likelihood = $row.find('input.likelihood').val();
         // Check if likelihood is greater than three or less than one and if so
         // reset the field to empty.
         // @todo: Put in some kind of message.
         if (likelihood > 3 || likelihood < 1) {
-          $row.find('.form-item.likelihood input').val('');
+          $row.find('input.likelihood').val('');
           likelihood = 0;
         }
         // Calculate the total.
         total = parseFloat(severity * likelihood);
         // If the total isn't zero then update the significance field value.
         if (total != 0) {
-          $row.find('.form-item.significance input').val(total);
+          $row.find('input.significance').val(total);
         }
       });
     }
