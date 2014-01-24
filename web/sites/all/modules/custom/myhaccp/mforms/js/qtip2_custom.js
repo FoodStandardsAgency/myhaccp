9282e4a1321881f2b78fb0ead5b80388124b0978
Tanc <mail@tanc.me>
2014-01-24 16:18:19 +0000

    Removed debug message.

--- web/sites/all/modules/custom/myhaccp/mforms/js/qtip2_custom.js
+++ web/sites/all/modules/custom/myhaccp/mforms/js/qtip2_custom.js
@@ -7,7 +7,7 @@
       $('.help-tip').each(function() {
         // Check if there is a help-text element to render.
         var help_text = $(this).parents('.form-item, th, div.label').children('.help-text');
-        console.log(help_text);
+        // If we have some help text then move on and add the tooltip.
         if (help_text.length > 0) {
           // Disable the usual click event on the button.
           $(this).click(false);
