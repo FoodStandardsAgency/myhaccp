95e7ba5adf8e6cbf6ec0426a1f537b6ae3b553d1
Hedley Smith <hedley@agilecollective.com>
2014-02-21 11:36:59 +0000

    Prep stage G

--- web/sites/all/modules/custom/myhaccp/mforms/preparatory/preparatory_g/preparatory_g.tpl.php
+++ web/sites/all/modules/custom/myhaccp/mforms/preparatory/preparatory_g/preparatory_g.tpl.php
@@ -5,5 +5,8 @@
  */
 ?>
 
-<h3><?php // print $a_1_title ?></h3>
-<?php // print $a_1; ?>
+<h2>Process flow and confirmation</h2>
+<p class="intro">We have completed a flow diagram covering all steps in the manufacturing process of this product.</p>
+
+<h3>A copy of our flow diagram can be obtained from the following location:</h3>
+<?php print $g_3; ?>
