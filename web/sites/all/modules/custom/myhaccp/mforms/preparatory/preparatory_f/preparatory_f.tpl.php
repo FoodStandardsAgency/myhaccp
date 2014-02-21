728bc097c5af1a2bab532628ee4118e30e6f1e94
Hedley Smith <hedley@agilecollective.com>
2014-02-21 11:04:40 +0000

    Starting Prep F

--- web/sites/all/modules/custom/myhaccp/mforms/preparatory/preparatory_f/preparatory_f.tpl.php
+++ web/sites/all/modules/custom/myhaccp/mforms/preparatory/preparatory_f/preparatory_f.tpl.php
@@ -5,5 +5,34 @@
  */
 ?>
 
-<h3><?php // print $a_1_title ?></h3>
-<?php // print $a_1; ?>
+<section class="page">
+  <h2>Intended Use</h2>
+
+  <h4>Name of the product</h4>
+  <p><?php print $f_1; ?></p>
+
+  <h4>Is the product designed for the young?</h4>
+  <p><?php print $f_2; ?></p>
+
+  <h4>Is the product designed for the elderly?</h4>
+  <p><?php print $f_3; ?></p>
+
+  <h4>Is the product designed for pregnant ladies?</h4>
+  <p><?php print $f_4; ?></p>
+
+  <h4>Is the product designed for the immunosuppressed / immunocompromised?</h4>
+  <p><?php print $f_5; ?></p>
+
+  <h4>Is the product suitable for people with allergies or intolerances?</h4>
+  <p><?php print $f_6; ?></p>
+
+  <h4>Are there any other groups of people that may consume your product that require consideration?</h4>
+  <p><?php print $f_7; ?></p>
+
+  <h4>Is it possible that your product could be consumed by someone other than those for whom it is intended?</h4>
+  <p><?php print $f_8; ?></p>
+
+  <h4>Detail below how you have considered the likely abuse / unintended use of the product by the consumer. ?</h4>
+  <p><?php print $f_9; ?></p>
+
+</section>
