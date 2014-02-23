2ea08fa6990e7f74b573674997530dfdd638d5a4
Hedley Smith <hedley@agilecollective.com>
2014-02-23 13:19:43 +0000

    First pass at Principle 1 and 1-2

--- web/sites/all/modules/custom/myhaccp/mforms/principle/principle_1_1/principle_1_1.tpl.php
+++ web/sites/all/modules/custom/myhaccp/mforms/principle/principle_1_1/principle_1_1.tpl.php
@@ -5,5 +5,7 @@
  */
 ?>
 
-<h3><?php // print $a_1_title ?></h3>
-<?php // print $a_1; ?>
+<h2>Identify and list potential hazards.</h2>
+
+
+<?php print $p_1_hazard_tables; ?>
--- web/sites/all/modules/custom/myhaccp/mforms/principle/principle_1_2/principle_1_2.tpl.php
+++ web/sites/all/modules/custom/myhaccp/mforms/principle/principle_1_2/principle_1_2.tpl.php
@@ -5,5 +5,12 @@
  */
 ?>
 
-<h3><?php // print $a_1_title ?></h3>
-<?php // print $a_1; ?>
+<section class="page">
+  <h2>Conduct a hazard analysis</h2>
+
+  <?php print $p_1_2_hazard_table ?>
+
+  <p><?php //print $d_1_team_skills ?></p>
+
+</section>
+
