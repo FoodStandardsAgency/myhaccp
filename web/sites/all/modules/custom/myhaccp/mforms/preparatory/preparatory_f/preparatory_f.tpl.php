56527e811b40e0d11e46ab16f7d16e53ae8d58bb
Hedley Smith <hedley@agilecollective.com>
2014-02-26 13:13:01 +0000

    Tidying up Prep Stages

--- web/sites/all/modules/custom/myhaccp/mforms/preparatory/preparatory_f/preparatory_f.tpl.php
+++ web/sites/all/modules/custom/myhaccp/mforms/preparatory/preparatory_f/preparatory_f.tpl.php
@@ -10,19 +10,33 @@
   <h4>Name of the product:</h4>
   <p><?php print $f_1; ?></p>
 
-  <p><?php print $f_2; ?></p>
+  <?php if ($f_2): ?>
+    <p><?php print $f_2; ?></p>
+  <?php endif; ?>
 
-  <p><?php print $f_3; ?></p>
+  <?php if ($f_3): ?>
+    <p><?php print $f_3; ?></p>
+  <?php endif; ?>
 
-  <p><?php print $f_4; ?></p>
+  <?php if ($f_4): ?>
+    <p><?php print $f_4; ?></p>
+  <?php endif; ?>
 
-  <p><?php print $f_5; ?></p>
+  <?php if ($f_5): ?>
+    <p><?php print $f_5; ?></p>
+  <?php endif; ?>
 
-  <p><?php print $f_6; ?></p>
+  <?php if ($f_6): ?>
+    <p><?php print $f_6; ?></p>
+  <?php endif; ?>
 
-  <p><?php print $f_7; ?></p>
+  <?php if ($f_7): ?>
+    <p><?php print $f_7; ?></p>
+  <?php endif; ?>
 
-  <p><?php print $f_8; ?></p>
+  <?php if ($f_8): ?>
+    <p><?php print $f_8; ?></p>
+  <?php endif; ?>
 
   <h4>We have considered the likely abuse / unintended use of the product by the consumer in the following ways:</h4>
   <p><?php print $f_9; ?></p>
