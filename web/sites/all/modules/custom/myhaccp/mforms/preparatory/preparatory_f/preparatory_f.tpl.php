54c0a7c9d1cecb5f502d71385fb61f6adf451f13
Hedley Smith <hedley@agilecollective.com>
2014-03-05 15:23:02 +0000

    Fixing P6 GIT issues, adding checks to prep stages

--- web/sites/all/modules/custom/myhaccp/mforms/preparatory/preparatory_f/preparatory_f.tpl.php
+++ web/sites/all/modules/custom/myhaccp/mforms/preparatory/preparatory_f/preparatory_f.tpl.php
@@ -10,31 +10,31 @@
   <h4>Name of the product:</h4>
   <p><?php print $f_1; ?></p>
 
-  <?php if ($f_2): ?>
+  <?php if(!empty($f_2)): ?>
     <p><?php print $f_2; ?></p>
   <?php endif; ?>
 
-  <?php if ($f_3): ?>
+  <?php if(!empty($f_3)): ?>
     <p><?php print $f_3; ?></p>
   <?php endif; ?>
 
-  <?php if ($f_4): ?>
+  <?php if(!empty($f_4)): ?>
     <p><?php print $f_4; ?></p>
   <?php endif; ?>
 
-  <?php if ($f_5): ?>
+  <?php if(!empty($f_5)): ?>
     <p><?php print $f_5; ?></p>
   <?php endif; ?>
 
-  <?php if ($f_6): ?>
+  <?php if(!empty($f_6)): ?>
     <p><?php print $f_6; ?></p>
   <?php endif; ?>
 
-  <?php if ($f_7): ?>
+  <?php if(!empty($f_7)): ?>
     <p><?php print $f_7; ?></p>
   <?php endif; ?>
 
-  <?php if ($f_8): ?>
+  <?php if(!empty($f_8)): ?>
     <h4><?php print $f_8_title; ?></h4>
     <p><?php print $f_8; ?></p>
   <?php endif; ?>
