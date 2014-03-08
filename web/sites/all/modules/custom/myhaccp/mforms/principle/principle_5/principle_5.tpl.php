cb9f4da1d13594f559b04f445f9a5976d1bddb6a
Hedley Smith <hedley@agilecollective.com>
2014-03-08 10:19:35 +0000

    Stopping table breaking in output PDF for HACCP Plan

--- web/sites/all/modules/custom/myhaccp/mforms/principle/principle_5/principle_5.tpl.php
+++ web/sites/all/modules/custom/myhaccp/mforms/principle/principle_5/principle_5.tpl.php
@@ -4,7 +4,7 @@
  * Stage output template.
  */
 ?>
-<section class="page">
+<section class="page haccp-plan">
   <h1>HACCP plan</h1>
   <?php if(!empty($p_5_table)): ?>
     <?php print $p_5_table; ?>
