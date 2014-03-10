17a35967fd636816de784512512438111e4a2b75
Hedley Smith <hedley@agilecollective.com>
2014-03-10 18:05:24 +0000

    Flow diagram in output PDF

--- web/sites/all/modules/custom/myhaccp/mforms/preparatory/preparatory_g/preparatory_g.tpl.php
+++ web/sites/all/modules/custom/myhaccp/mforms/preparatory/preparatory_g/preparatory_g.tpl.php
@@ -12,8 +12,15 @@
   <?php endif; ?>
 
   <?php if(!empty($flow_diagram_file)) : ?>
-    <?php print render($flow_diagram_file); ?>
+    <?php //print render($flow_diagram_file); ?>
   <?php endif; ?>
+
+  <?php if(!empty($flow_diagram_data)) : ?>
+    <img src="data:image/gif;base64,<?php print $flow_diagram_data; ?>" />
+    <?php //print render($flow_diagram_file); ?>
+  <?php endif; ?>
+
+  <?php print $imagestring; ?>
 
   <?php if (!empty($g_3)): ?>
     <h3>A copy of our flow diagram can be obtained from the following location:</h3>
