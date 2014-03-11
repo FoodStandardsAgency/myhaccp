1379ea719ea87defc7f3c54ee778fdeec0268771
Hedley Smith <hedley@agilecollective.com>
2014-03-11 14:23:51 +0000

    Styling for flow diagram and Principle 3 page in PDF

--- web/sites/all/modules/custom/myhaccp/mforms/principle/principle_3/principle_3.tpl.php
+++ web/sites/all/modules/custom/myhaccp/mforms/principle/principle_3/principle_3.tpl.php
@@ -4,3 +4,12 @@
  * Stage output template.
  */
 ?>
+
+<section>
+  <h1>Critical limits for the CCPs</h1>
+  <p class="intro">The following critical limits have been established for the CCPs.</p>
+  <?php if(!empty($p_3_table)): ?>
+    <?php print $p_3_table; ?>
+  <?php endif; ?>
+
+</section>
