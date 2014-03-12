b007b3745f0576aff44b40dc870506eb1e621b87
Hedley Smith <hedley@agilecollective.com>
2014-03-12 12:09:48 +0000

    User account pages styling

--- web/sites/all/themes/custom/myhaccp/page.tpl.php
+++ web/sites/all/themes/custom/myhaccp/page.tpl.php
@@ -1,9 +1,3 @@
-<?php
-//kpr(get_defined_vars());
-//kpr($theme_hook_suggestions);
-//template naming
-//page--[CONTENT TYPE].tpl.php
-?>
 <?php if( theme_get_setting('cleanup_poorthemers_helper') ){ ?>
 <!-- page.tpl.php-->
 <?php } ?>
@@ -71,11 +65,6 @@
     <?php endif; ?>
 
     <div role="main" id="main-content">
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
 
       <?php if ($action_links): ?>
         <ul class="action-links"><?php print render($action_links); ?></ul>
@@ -84,6 +73,12 @@
       <?php if (isset($tabs['#primary'][0]) || isset($tabs['#secondary'][0])): ?>
         <nav class="tabs"><?php print render($tabs); ?></nav>
       <?php endif; ?>
+
+      <?php print render($title_prefix); ?>
+      <?php if ($title): ?>
+        <h1><?php print $title; ?></h1>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
 
       <?php if($page['highlighted'] OR $messages){ ?>
         <div class="drupal-messages">
