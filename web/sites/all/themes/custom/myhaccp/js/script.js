3043048381b97f0ddb39e9ac79108d8dcf50ff20
Hedley Smith <hedley@agilecollective.com>
2014-03-12 12:54:54 +0000

    Reverting back to targetting the anchor element for the modal popup menu (from merge conflict)

--- web/sites/all/themes/custom/myhaccp/js/script.js
+++ web/sites/all/themes/custom/myhaccp/js/script.js
@@ -3,8 +3,8 @@
     attach: function(context) {
       $('.help-menu a').click(function(e) {
         var link = $(this).attr('href');
-        $('.help-menu a[href$="' + link + '"]').parents('nav').find('li').removeClass("active");
-        $('.help-menu a[href$="' + link + '"]').parent().addClass("active");
+        $('.help-menu ul li a').removeClass("active");
+        $('.help-menu a[href$="' + link + '"]').addClass("active");
         $(link).removeClass("hidden").siblings("article").addClass("hidden");
         $(".ui-widget-overlay").scrollTop(0);
       });
