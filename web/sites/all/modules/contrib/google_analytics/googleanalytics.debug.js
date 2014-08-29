64e2b2540d38393e0c5cd3ca59563cf5f26ebeac
tanc <mail@tanc.me>
2014-08-29 10:50:46 +0100

    Updated google analytics module to 2.0 stable.

--- web/sites/all/modules/contrib/google_analytics/googleanalytics.debug.js
+++ web/sites/all/modules/contrib/google_analytics/googleanalytics.debug.js
@@ -1,52 +1,183 @@
+(function ($) {
+
+Drupal.googleanalytics = {};
+
+$(document).ready(function() {
+
+  // Attach mousedown, keyup, touchstart events to document only and catch
+  // clicks on all elements.
+  $(document.body).bind("mousedown keyup touchstart", function(event) {
+    console.group("Running Google Analytics for Drupal.");
+    console.info(event);
+
+    // Catch the closest surrounding link of a clicked element.
+    $(event.target).closest("a,area").each(function() {
+      console.info("Element '%o' has been detected. Link '%s' found.", this, this.href);
+
+      // Is the clicked URL internal?
+      if (Drupal.googleanalytics.isInternal(this.href)) {
+        // Skip 'click' tracking, if custom tracking events are bound.
+        if ($(this).is('.colorbox')) {
+          // Do nothing here. The custom event will handle all tracking.
+          console.info("Click on .colorbox item has been detected.");
+        }
+        // Is download tracking activated and the file extension configured for download tracking?
+        else if (Drupal.settings.googleanalytics.trackDownload && Drupal.googleanalytics.isDownload(this.href)) {
+          // Download link clicked.
+          console.info("Download url '%s' has been found. Tracked download as extension '%s'.", Drupal.googleanalytics.getPageUrl(this.href), Drupal.googleanalytics.getDownloadExtension(this.href).toUpperCase());
+          ga("send", "event", "Downloads", Drupal.googleanalytics.getDownloadExtension(this.href).toUpperCase(), Drupal.googleanalytics.getPageUrl(this.href));
+        }
+        else if (Drupal.googleanalytics.isInternalSpecial(this.href)) {
+          // Keep the internal URL for Google Analytics website overlay intact.
+          console.info("Click on internal special link '%s' has been tracked.", Drupal.googleanalytics.getPageUrl(this.href));
+          ga("send", "pageview", { page: Drupal.googleanalytics.getPageUrl(this.href) });
+        }
+        else {
+          // e.g. anchor in same page or other internal page link
+          console.info("Click on internal link '%s' detected, but not tracked by click.", this.href);
+        }
+      }
+      else {
+        if (Drupal.settings.googleanalytics.trackMailto && $(this).is("a[href^='mailto:'],area[href^='mailto:']")) {
+          // Mailto link clicked.
+          console.info("Click on e-mail '%s' has been tracked.", this.href.substring(7));
+          ga("send", "event", "Mails", "Click", this.href.substring(7));
+        }
+        else if (Drupal.settings.googleanalytics.trackOutbound && this.href.match(/^\w+:\/\//i)) {
+          if (Drupal.settings.googleanalytics.trackDomainMode != 2 && !Drupal.googleanalytics.isCrossDomain(this.hostname, Drupal.settings.googleanalytics.trackCrossDomains)) {
+            // External link clicked / No top-level cross domain clicked.
+            console.info("Outbound link '%s' has been tracked.", this.href);
+            ga("send", "event", "Outbound links", "Click", this.href);
+          }
+          else {
+            console.info("Internal link '%s' clicked, not tracked.", this.href);
+          }
+        }
+      }
+    });
+
+    console.groupEnd();
+  });
+
+  // Track hash changes as unique pageviews, if this option has been enabled.
+  if (Drupal.settings.googleanalytics.trackUrlFragments) {
+    window.onhashchange = function() {
+      console.info("Track URL '%s' as pageview. Hash '%s' has changed.", location.pathname + location.search + location.hash, location.hash);
+      ga('send', 'pageview', location.pathname + location.search + location.hash);
+    }
+  }
+
+  // Colorbox: This event triggers when the transition has completed and the
+  // newly loaded content has been revealed.
+  $(document).bind("cbox_complete", function () {
+    var href = $.colorbox.element().attr("href");
+    if (href) {
+      console.info("Colorbox transition to url '%s' has been tracked.", Drupal.googleanalytics.getPageUrl(href));
+      ga("send", "pageview", { page: Drupal.googleanalytics.getPageUrl(href) });
+    }
+  });
+
+});
+
 /**
- *  This file is for developers only.
+ * Check whether the hostname is part of the cross domains or not.
  *
- *  This tests are made for the javascript functions used in GA module.
- *  These tests verify if the return values are properly working.
+ * @param string hostname
+ *   The hostname of the clicked URL.
+ * @param array crossDomains
+ *   All cross domain hostnames as JS array.
  *
- *  Hopefully this can be added somewhere else once Drupal core has JavaScript
- *  unit testing integrated.
+ * @return boolean
  */
+Drupal.googleanalytics.isCrossDomain = function (hostname, crossDomains) {
+  /**
+   * jQuery < 1.6.3 bug: $.inArray crushes IE6 and Chrome if second argument is
+   * `null` or `undefined`, http://bugs.jquery.com/ticket/10076,
+   * https://github.com/jquery/jquery/commit/a839af034db2bd934e4d4fa6758a3fed8de74174
+   *
+   * @todo: Remove/Refactor in D8
+   */
+  if (!crossDomains) {
+    return false;
+  }
+  else {
+    return $.inArray(hostname, crossDomains) > -1 ? true : false;
+  }
+}
 
-// JavaScript debugging
-var base_url = window.location.protocol + '//' + window.location.host; 
-console.dir(Drupal);
+/**
+ * Check whether this is a download URL or not.
+ *
+ * @param string url
+ *   The web url to check.
+ *
+ * @return boolean
+ */
+Drupal.googleanalytics.isDownload = function (url) {
+  var isDownload = new RegExp("\\.(" + Drupal.settings.googleanalytics.trackDownloadExtensions + ")([\?#].*)?$", "i");
+  return isDownload.test(url);
+}
 
-console.group("Test 'isDownload':");
-console.log("Check if '/node/8' url is a file download. Expected: false, Result: %s.", Drupal.googleanalytics.isDownload(base_url + '/node/8'));
-console.log("Check if '/files/foo1.zip' url is a file download. Expected: true, Result: %s.", Drupal.googleanalytics.isDownload(base_url + '/files/foo1.zip'));
-console.log("Check if '/files/foo2.ddd' url is a file download. Expected: false, Result: %s.", Drupal.googleanalytics.isDownload(base_url + '/files/foo2.ddd'));
-console.groupEnd();
+/**
+ * Check whether this is an absolute internal URL or not.
+ *
+ * @param string url
+ *   The web url to check.
+ *
+ * @return boolean
+ */
+Drupal.googleanalytics.isInternal = function (url) {
+  var isInternal = new RegExp("^(https?):\/\/" + window.location.host, "i");
+  return isInternal.test(url);
+}
 
-console.group("Test 'isInternal':");
-console.log("Check if base_url + '/node/1#foo=bar' url is internal. Expected: true, Result: %s.", Drupal.googleanalytics.isInternal(base_url + '/node/1#foo=bar'));
-console.log("Check if base_url + '/node/2' url is internal. Expected: true, Result: %s.", Drupal.googleanalytics.isInternal(base_url + '/node/2'));
-console.log("Check if base_url + '/go/foo' url is internal. Expected: true, Result: %s.", Drupal.googleanalytics.isInternal(base_url + '/go/foo'));
-console.log("Check if 'http://example.com/node/3' url is internal. Expected: false, Result: %s.", Drupal.googleanalytics.isInternal('http://example.com/node/3'));
-console.groupEnd();
+/**
+ * Check whether this is a special URL or not.
+ *
+ * URL types:
+ *  - gotwo.module /go/* links.
+ *
+ * @param string url
+ *   The web url to check.
+ *
+ * @return boolean
+ */
+Drupal.googleanalytics.isInternalSpecial = function (url) {
+  var isInternalSpecial = new RegExp("(\/go\/.*)$", "i");
+  return isInternalSpecial.test(url);
+}
 
-console.group("Test 'isInternalSpecial':");
-console.log("Check if base_url + '/go/foo' url is internal special. Expected: true, Result: %s.", Drupal.googleanalytics.isInternalSpecial(base_url + '/go/foo'));
-console.log("Check if base_url + '/node/1' url is internal special. Expected: false, Result: %s.", Drupal.googleanalytics.isInternalSpecial(base_url + '/node/1'));
-console.groupEnd();
+/**
+ * Extract the relative internal URL from an absolute internal URL.
+ *
+ * Examples:
+ * - http://mydomain.com/node/1 -> /node/1
+ * - http://example.com/foo/bar -> http://example.com/foo/bar
+ *
+ * @param string url
+ *   The web url to check.
+ *
+ * @return string
+ *   Internal website URL
+ */
+Drupal.googleanalytics.getPageUrl = function (url) {
+  var extractInternalUrl = new RegExp("^(https?):\/\/" + window.location.host, "i");
+  return url.replace(extractInternalUrl, '');
+}
 
-console.group("Test 'getPageUrl':");
-console.log("Get absolute internal url from full qualified url. Expected: '/node/1', Result: '%s'.", Drupal.googleanalytics.getPageUrl(base_url + '/node/1'));
-console.log("Get absolute internal url from absolute url. Expected: '/node/1', Result: '%s'.", Drupal.googleanalytics.getPageUrl('/node/1'));
-console.log("Get full qualified external url. Expected: 'http://example.com/node/2', Result: '%s'.", Drupal.googleanalytics.getPageUrl('http://example.com/node/2'));
-console.groupEnd();
+/**
+ * Extract the download file extension from the URL.
+ *
+ * @param string url
+ *   The web url to check.
+ *
+ * @return string
+ *   The file extension of the passed url. e.g. "zip", "txt"
+ */
+Drupal.googleanalytics.getDownloadExtension = function (url) {
+  var extractDownloadextension = new RegExp("\\.(" + Drupal.settings.googleanalytics.trackDownloadExtensions + ")([\?#].*)?$", "i");
+  var extension = extractDownloadextension.exec(url);
+  return (extension === null) ? '' : extension[1];
+}
 
-console.group("Test 'getDownloadExtension':");
-console.log("Get extension of download filename. Expected: 'zip', Result: '%s'.", Drupal.googleanalytics.getDownloadExtension(base_url + '/files/foo1.zip'));
-console.log("Get empty extension if not a download extension. Expected: '', Result: '%s'.", Drupal.googleanalytics.getDownloadExtension(base_url + '/files/foo2.dddd'));
-console.groupEnd();
-
-// List of top-level domains: example.com, example.net
-console.group("Test 'isCrossDomain' (requires cross domain configuration):");
-console.dir(Drupal.settings.google_analytics.trackCrossDomains);
-console.log("Check if url is in cross domain list. Expected: true, Result: %s.", Drupal.googleanalytics.isCrossDomain('example.com', Drupal.settings.google_analytics.trackCrossDomains));
-console.log("Check if url is in cross domain list. Expected: true, Result: %s.", Drupal.googleanalytics.isCrossDomain('example.net', Drupal.settings.google_analytics.trackCrossDomains));
-console.log("Check if url is in cross domain list. Expected: false, Result: %s.", Drupal.googleanalytics.isCrossDomain('www.example.com', Drupal.settings.google_analytics.trackCrossDomains));
-console.log("Check if url is in cross domain list. Expected: false, Result: %s.", Drupal.googleanalytics.isCrossDomain('www.example.net', Drupal.settings.google_analytics.trackCrossDomains));
-console.groupEnd();
-
+})(jQuery);
